86f0b13d325d75b19d716d60bb09435d64b05f44
Alvin <alvinchandy@gmail.com>
2021-12-12 16:33:36 -0500

    removed origins from * to lclhost and commented out include in frontend

--- twitter-server-master/src/main/java/com/example/twitterserver/tweets/TweetDao.java
+++ twitter-server-master/src/main/java/com/example/twitterserver/tweets/TweetDao.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TweetDao {
     @Autowired
     UserRepository userRepository;
