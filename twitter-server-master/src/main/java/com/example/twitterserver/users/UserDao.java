4b810fa8ff4e87776d2f6142d4d8419cbddc685d
Jason Julien <jasonjulien@jasons-mbp.lan>
2021-12-11 23:19:55 -0500

    logout

--- twitter-server-master/src/main/java/com/example/twitterserver/users/UserDao.java
+++ twitter-server-master/src/main/java/com/example/twitterserver/users/UserDao.java
@@ -24,18 +24,41 @@ public class UserDao {
             @PathVariable("userId") Integer userId) {
         return userRepository.findById(userId).get();
     }
+    //order goes register => login => loggedIn
 
-    //login
+    //registers a user into the database
     @PostMapping("/api/register")
     public User register(@RequestBody User user, HttpSession session){
         session.setAttribute("currentUser", user);
+        userRepository.save(user);
         return user;
     }
-
+    //gets?? who logs in (why is not a get endpoint --- can only think of this for security purposes)
     @PostMapping("/api/loggedin")
     public User loggedIn(HttpSession session){
        return  (User) session.getAttribute("currentUser");
     }
+    //need to change this logic that it asks for a match on the userId? idk how
+    @PostMapping("api/login")
+    public User login(@RequestBody User loginUser, HttpSession session)
+    {
+        for(User user: userRepository.findAll()){
+            if(user.getUser_name().equals(loginUser.getUser_name())){
+                session.setAttribute("currentUser", user);
+                return user;
+            }
+        }
+        return null;
+
+    }
+
+    @PostMapping("api/logout")
+    public void logout( HttpSession session)
+    {
+        session.invalidate();
+
+    }
+
 
 
 
